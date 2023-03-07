d0d92ffd9fec5e51f84e4a7b3020ee4c93773b6f
Harsh Soni <algebra.soni@gmail.com>
2023-03-07 12:14:16 +0000

    char bool int float

---
+++ 06.php
@@ -0,0 +1,14 @@
+<!-- print Character Boolean, Integer and Floating values using single variable.  -->
+<?php
+  $myVariable = 'a'; // Character
+  echo $myVariable . '<br>';
+  
+  $myVariable = true; // Boolean
+  echo $myVariable . '<br>';
+  
+  $myVariable = 123; // Integer
+  echo $myVariable . '<br>';
+  
+  $myVariable = 3.14; // Floating-point number
+  echo $myVariable . '<br>';
+?>
