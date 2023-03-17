0c4c18c9469003f97a2ea387504d0e267c649410
Harsh Soni <algebra.soni@gmail.com>
2023-03-17 12:51:11 +0000

    create session cookie

--- 13.php
+++ 13.php
@@ -1,1 +1,16 @@
 <!-- create session cookie -->
+
+<?php
+// Set the session cookie parameters
+session_set_cookie_params(3600, '/', 'localhost', false, true);
+
+// Start the session
+session_start();
+
+// Set a session variable
+$_SESSION['username'] = 'JohnDoe';
+
+// Display the session ID and the value of the session variable
+echo 'Session ID: ' . session_id() . '<br>';
+echo 'Username: ' . $_SESSION['username'];
+?>
