a80210884240664da9aa034eccce1daa4b7b5d84
Harsh Soni <algebra.soni@gmail.com>
2023-03-17 12:22:34 +0000

    3600

--- 11.php
+++ 11.php
@@ -4,7 +4,7 @@
 <?php
 $cookie_name = "user";
 $cookie_value = "John Doe";
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+setcookie($cookie_name, $cookie_value, time() + (3600), "/"); // 86400 = 1 day
 ?>
 <html>
 
