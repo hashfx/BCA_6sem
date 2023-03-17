9d7490de5fb3e708d3b67464ffa45c1b04cb97f0
Harsh Soni <algebra.soni@gmail.com>
2023-03-17 13:26:02 +0000

    persistent cookie

--- 14.php
+++ 14.php
@@ -1,1 +1,31 @@
 <!-- create persistent cookie -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Persistent Cookie</title>
+</head>
+
+<body>
+    <form method="post">
+        <label for="username">Enter your username:</label>
+        <input type="text" id="username" name="username">
+        <button type="submit" name="create_cookie">Create Cookie</button>
+    </form>
+
+    <?php
+    if (isset($_POST['create_cookie'])) {
+        $username = $_POST['username'];
+
+        // Set cookie expiration to 1 year from now
+        $expiration = time() + (3600);
+
+        // Set the cookie
+        setcookie('username', $username, $expiration, '/');
+
+        echo "<p>Cookie created successfully.</p>";
+    }
+    ?>
+</body>
+
+</html>
