05152b1eec597a297f11a74c95b32189e69df3f1
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2023-03-17 15:19:50 +0530

    login logic

--- 1/login.php
+++ 1/login.php
@@ -2,3 +2,21 @@
 include 'nav.php';
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body>
+	<h1>Login Form</h1>
+	<form method="post" action="login_redir.php">
+		<label for="username">Username:</label>
+		<input type="text" id="username" name="username" required><br><br>
+		
+		<label for="password">Password:</label>
+		<input type="password" id="password" name="password" required><br><br>
+		
+		<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
