ea6f3346f0b4325323428455c6c8d12361d381e3
Harsh Soni <69109482+hashfx@users.noreply.github.com>
2023-03-17 15:10:52 +0530

    registration logic

--- 1/contact.php
+++ 1/contact.php
@@ -2,3 +2,27 @@
 include 'nav.php';
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration Page</title>
+</head>
+<body>
+	<h1>Registration Form</h1>
+	<form method="post" action="register.php">
+		<label for="username">Username:</label>
+		<input type="text" id="username" name="username" required><br><br>
+		
+		<label for="password">Password:</label>
+		<input type="password" id="password" name="password" required><br><br>
+		
+		<label for="confirm_password">Confirm Password:</label>
+		<input type="password" id="confirm_password" name="confirm_password" required><br><br>
+		
+		<label for="email">Email:</label>
+		<input type="email" id="email" name="email" required><br><br>
+		
+		<input type="submit" value="Submit">
+	</form>
+</body>
+</html>
