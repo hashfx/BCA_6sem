e39ae3fd964a8130d999d709db04adb45fa00e8a
hashfx <algebra.soni@gmail.com>
2023-03-17 19:30:42 +0530

    set cookies

--- 12.php
+++ 12.php
@@ -8,12 +8,16 @@
 
 <body>
     <?php
+    $cookie_name = "FAANG";
+    $cookie_value = "Google";
+    setcookie($cookie_name, $cookie_value, time() + (3600), "/");
+
     // check if the "name" cookie is set
-    if (isset($_COOKIE['name'])) {
+    if (isset($_COOKIE['cookie_name'])) {
         // retrieve the value of the "name" cookie
-        $name = $_COOKIE['name'];
+        $name = $_COOKIE['cookie_name'];
         // display the value of the "name" cookie
-        echo "Hello, $name!";
+        echo "Hello ".$name;
     } else {
         // if the "name" cookie is not set, display a message
         echo "Cookie is not set.";
