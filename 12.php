f7874c895bdd9191b5ed9a3fb79304b8b222edcc
Harsh Soni <algebra.soni@gmail.com>
2023-03-17 12:34:59 +0000

    access cookies in php

--- 12.php
+++ 12.php
@@ -1,1 +1,24 @@
 <!-- Access cookie data and display it -->
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Accessing Cookies in PHP</title>
+</head>
+
+<body>
+    <?php
+    // check if the "name" cookie is set
+    if (isset($_COOKIE['name'])) {
+        // retrieve the value of the "name" cookie
+        $name = $_COOKIE['name'];
+        // display the value of the "name" cookie
+        echo "Hello, $name!";
+    } else {
+        // if the "name" cookie is not set, display a message
+        echo "Cookie is not set.";
+    }
+    ?>
+</body>
+
+</html>
